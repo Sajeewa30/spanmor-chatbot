80b9d46b499a5b17fc73be6443fcb90e788d04b9
Sajeewa-Laptop <sajeewadilshan30@gmail.com>
2025-11-04 11:43:04 +0530

    renamed chatbot component

---
+++ components/Chatbot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+"use client";
+
+const Chatbot = () => {
+    return (
+        <div className="chatbot-container">
+            {/* Chatbot content will go here */}
+        </div>
+    );
+};
+
+export default Chatbot;
